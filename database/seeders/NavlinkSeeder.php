a4648e7af196078e15f7bf50ed294cfa0607e8c2
Florence <monnaie.florence@gmail.com>
2021-02-25 12:01:49 +0100

    tables created

---
+++ database/seeders/NavlinkSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class NavlinkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ resources/views/pages/portfolio-details.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.main')
+@section('content')
+<main id="main">
+    @include('partials.portfolio-details')
+</main>
+@endsection
